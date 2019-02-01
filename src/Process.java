85b6bcbe1df1ed8011feefed1077019c5bf57750
juthaporn <jutha.1995@gmail.com>
2019-02-01 17:46:41 +0700

    process

--- src/Process.java
+++ src/Process.java
@@ -7,7 +7,25 @@ public class Process {
 		this.score = score;
 	}
 	public char calculate() {
-		return (Character) null;
+		char grade;
+		if(score>=80) {
+			grade = 'A';
+		}else if(score>=75){
+            grade = 'B';
+        }
+        else if(score >= 70){
+            grade = 'B';
+        }
+        else if(score >= 60){
+            grade = 'C';
+        }
+        else if(score >= 50){
+            grade = 'D';
+        }
+        else{
+            grade = 'F';
+        }
+		return grade;
 	}
 	
 }
